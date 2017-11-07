7f904e35dbbe0237bd1dc8a14b67803394d5d75b
Sadot Cortes <sadotcortes@scoorm.me>
2017-11-07 12:47:44 -0600

    [app/routes/index] purge

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,9 @@
 let express = require('express');
-let path = require('path');
 let router = express.Router();
 
 // HOMEPAGE - GET - localhost:3000/
 
 router.get('/', (req, res, next) => {
-  res.send('WELCOME TO PESITO');
 });
 
 module.exports = router;
