a6e4789ae6a1ffb9c807cb38e0bd11876a48d7a0
Sadot Cortes <sadotcortes@scoorm.me>
2017-11-06 21:33:27 -0600

    [app/routes/index.js] ++ routes/index updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
 let express = require('express');
+let path = require('path');
 let router = express.Router();
 
 // HOMEPAGE - GET - localhost:3000/
 
 router.get('/', (req, res, next) => {
-  res.send('HOMEPAGE');
+  res.send('WELCOME TO PESITO');
 });
 
 module.exports = router;
