b6f11938253159f8078d6c8e5b2e26a288786f5b
Sadot Cortes <sadotcortes@scoorm.me>
2017-11-08 19:38:59 -0600

    Goals routes ++

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const params = require('strong-params');
 
 const indexRoute = require('./routes/index.js');
 const usersRoute = require('./routes/user.js');
+const goalsRoute = require('./routes/goals.js');
 
 const PORT = 3001;
 const app = express();
@@ -19,6 +20,7 @@ app.use(params.expressMiddleware());
 
 app.use('/', indexRoute);
 app.use('/api/users', usersRoute);
+app.use('/api/goals', goalsRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
